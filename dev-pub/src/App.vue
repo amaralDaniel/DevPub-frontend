f1fe7a0c4ea72eddb76f2656ac362962e9a6b6e6
Daniel Amaral <amaral94daniel@gmail.com>
2017-09-20 21:58:55 +0100

    init

---
+++ dev-pub/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div id="app">
+    <header>
+      <span>DevPub</span>
+      <div class="routes">
+        <a>/itHappened</a>
+        <a>/weSolved</a>
+        <a>/weBuilt</a>
+        <a>/faq</a>
+        <a>/subscribe</a>
+      </div>
+    </header>
+    <main>
+      <!-- <img src="./assets/logo.png" alt="Vue.js PWA"> -->
+      <!-- <router-view></router-view> -->
+      <Publication></Publication>
+      <Publication></Publication>
+      <Publication></Publication>
+    </main>
+  </div>
+</template>
+
+<script>
+import Publication from './components/Publication'
+export default {
+  name: 'app',
+  components: {
+    Publication
+  }
+}
+</script>
+
+<style lang="sass" >
+@import './styles/variables.scss'
+
+body
+  margin: 0
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  color: #2c3e50
+
+
+main
+  margin-top: 40px
+  display: block
+
+
+header
+  margin: 0
+  height: 56px
+  padding: 0 16px 0 24px
+  background-color: $main-green
+  color: $main-red
+
+
+header span
+  display: block
+  position: relative
+  font-size: 20px
+  line-height: 1
+  letter-spacing: .02em
+  font-weight: 900
+  box-sizing: border-box
+  padding-top: 16px
+
+
+header .routes
+  color: $main-red
+  display: flex
+  flex-direction: row
+  flex-wrap: nowrap
+  align-items: center
+  align-content: flex-start
+  float: right
+  font-size: 16px
+  line-height: 1
+  letter-spacing: .02em
+  font-weight: 700
+  box-sizing: border-box
+
+  a
+    padding-right: 10px
+
+</style>
---
+++ dev-pub/src/components/Publication.vue
@@ -0,0 +1,65 @@
+<template lang="html">
+  <div class="post">
+    <div class="title-user">
+      <a href="https://www.elastic.co/blog/psd2-architectures-with-the-elastic-stack" class=" title"> {{ title }}  </a>
+      <a class="user"> by {{ user }} </a>
+    </div>
+    <div class="abstract"> {{ abstract }} </div>
+    <div class="date"> {{ postDate }} </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      postDate: '17 September \'17',
+      title: 'PSD2: Modern Banking API Architectures with the Elastic Stack',
+      user: 'poster',
+      abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../styles/variables.scss'
+
+.post
+  margin: 5vh auto
+  width: 65%
+  // background-color: $main-orange
+  // border-radius: 5px
+  // border: 2px solid $main-green
+  padding: 5px
+
+  .title-user
+    width: 100%
+    margin: 5% auto
+    padding-bottom: 5px
+
+  .title
+    float: left
+    color: $main-red
+    font-weight: 900
+
+  .user
+    margin-left: 15px
+    margin: 0 0 0 2vh
+    font-weight: 700
+    color: $main-purple
+    float: right
+
+  .date
+    float: right
+    font-weight: 300
+    font-size: 10px
+    margin: 2vh auto
+
+  .abstract
+    font-size: 16px
+    font-weight: 400
+    margin: 0 auto
+    color: black
+
+</style>
