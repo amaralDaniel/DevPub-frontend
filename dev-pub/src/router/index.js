3b5a19fe8f7529c9f1a86ccad895f0d8abc8ec54
Daniel Amaral <amaral94daniel@gmail.com>
2017-10-05 18:24:53 +0100

    router paths defined

--- dev-pub/src/router/index.js
+++ dev-pub/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Publication from '@/components/Publication'
 
 Vue.use(Router)
 
@@ -10,12 +8,37 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: require('../components/Hello.vue')
     },
     {
       path: '/pub',
       name: 'Pub',
-      component: Publication
+      component: require('../components/Publication.vue')
+    },
+    {
+      path: '/ithappened',
+      name: 'ItHappened',
+      component: require('../components/ItHappened.vue')
+    },
+    {
+      path: '/wesolved',
+      name: 'WeSolved',
+      component: require('../components/WeSolved.vue')
+    },
+    {
+      path: '/webuilt',
+      name: 'WeBuilt',
+      component: require('../components/WeBuilt.vue')
+    },
+    {
+      path: '/faq',
+      name: 'Faq',
+      component: require('../components/Faq.vue')
+    },
+    {
+      path: '/subscribe',
+      name: 'Subscribe',
+      component: require('../components/Subscribe.vue')
     }
   ]
 })
